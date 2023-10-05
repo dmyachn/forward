b528f51046bddf43f0a106c20fc81a2ce313917e
Dmitry <d.myachn@gmail.com>
2023-10-05 22:23:43 +0300

    Initial Commit

---
+++ forward/forwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  forwardApp.swift
+//  forward
+//
+//  Created by Dmitiy Myachin on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct forwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
