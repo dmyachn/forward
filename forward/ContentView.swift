ee4814886cc2edaff7fba96fe93a32f82f43f629
Dmitry <d.myachn@gmail.com>
2023-10-06 07:44:12 +0300

    fuck

--- forward/ContentView.swift
+++ forward/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var array: [AnyView] = [
+        AnyView(Image(systemName: "play")
+            .resizable()
+            .transition(
+                .asymmetric(insertion: .move(edge: .leading).combined(with: .scale),
+                                    removal: .move(edge: .trailing).combined(with: .scale)))
+        ),
+
+        AnyView(Image(systemName: "play")
+            .resizable()
+            .foregroundColor(.yellow)
+            .transition(
+                .asymmetric(insertion: .move(edge: .leading).combined(with: .scale),
+                                    removal: .move(edge: .trailing).combined(with: .scale)))
+        ),
+        
+        AnyView(Image(systemName: "play.fill")
+            .resizable()
+            .foregroundColor(.yellow)
+            .transition(
+                .asymmetric(insertion: .move(edge: .leading).combined(with: .scale),
+                                    removal: .move(edge: .trailing).combined(with: .scale)))
+        )
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            withAnimation(.easeInOut){
+            let view = array.remove(at: 1)
+            array.insert(view , at: 0)
+            }
+        } label: {
+            HStack(spacing: 0){
+                ForEach(0..<2) {
+                    array[$0]
+                }.animation(.easeInOut, value: 10)
+            }.frame(width: 50, height: 30)
         }
-        .padding()
     }
 }
 
